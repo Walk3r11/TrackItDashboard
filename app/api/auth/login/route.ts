bfbb35970207097e0ee6e935619a46eed89be894
gagasslut <152082235+Walk3r11@users.noreply.github.com>
2025-12-21 14:00:03 +0200

    auth error

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: NextRequest) {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "lax",
-        maxAge: 60 * 60 * 24,
+        maxAge: 60 * 60,
       });
     }
 
